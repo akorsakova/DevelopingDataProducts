eea7868e909e720aa357e70d7aff0582cbe97e5d
Akorsakova <akorsakova@gmail.com>
2015-03-21 11:24:17 -0400

    adding shiny code

--- ChooseABankAssistant/server.R
+++ ChooseABankAssistant/server.R
@@ -1,21 +1,131 @@
+#define libraries
 library(shiny)
-url <- 'https://github.com/akorsakova/DevelopingDataProducts/blob/master/Data/Bank_accounts_and_services_complaints.csv'
-cfpb <- read.csv(url(url))
+library(ggplot2)
+library(plyr)
 
-# Define server logic required to draw a histogram
+  #load data to be used throughout the bank and clean it up
+  cfpb <- read.csv("data/Bank_accounts_and_services_complaints.csv",stringsAsFactors=FALSE) 
+  #get rid of the spaces in the column names
+  names(cfpb) <- sub(" ", "", names(cfpb))
+  #change date received to a date type and add a month year variable for graphing
+  cfpb$Date.received <- as.Date(cfpb$Date.received, "%m/%d/%Y")
+  cfpb$monthYr = format(cfpb$Date.received,format="%Y-%m")
+
+
 shinyServer(function(input, output) {
-  
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  
-  output$text1 <- renderText({ 
-    dim(cfpb)
+  #create drop down box with dynamic values from the data
+  output$choose_bank <- renderUI({
+    selectInput("bank", "Choose a Bank", as.list(sort(unique(cfpb$Company))))
   })
-  output$plot1 <- renderPlot({
-    hist(mtcars$mpg)
+  
+  #create first graph
+  output$freqPlot <- renderPlot({
+    #get frequency counts
+    counts <- data.frame(table(cfpb$Company))
+    names(counts) <- c("bank","count")
+
+    #get top 10 banks
+    largeNum <- head(counts[order(-counts$count), ],10)
+    topBank <- cfpb[,(names(cfpb) %in% c("Company", "Issue"))]
+    topBank <- subset(topBank, Company %in% largeNum$bank)
+    #get rid of rows without issue values
+    topBank[topBank == ""] <- NA
+    topBank<- topBank[complete.cases(topBank),]
+    
+    #get frequency counts for the total labels
+    totals <- ddply(topBank, .(Company), summarise, freq=length(Company))
+    
+    #build plot
+    p <- qplot(Company, data=topBank, geom="bar", fill=Issue) + 
+      xlab("Bank Name") + ylab("Total Number of Complants") +
+      ggtitle("Top 10 Banks with Highest Number of Complaints for 2014") +
+      theme(axis.text.x = element_text(angle = 25, hjust = 1))+
+      geom_text(data=totals,aes(label=freq,x=Company,y=freq),inherit.aes=FALSE)
+    
+    print(p)
   })
+  
+  #create second graph
+  output$linePlot <- renderPlot({
+    #get frequency counts
+    counts1 <- data.frame(table(cfpb$Company))
+    names(counts1) <- c("bank","count")
+    
+    #get top 10 banks
+    largeNum1 <- head(counts1[order(-counts1$count), ],10)
+    topBank1 <- cfpb[,(names(cfpb) %in% c("Company", "monthYr"))]
+    topBank1 <- subset(topBank1, Company %in% largeNum1$bank)
+
+    #get frequency counts for the total labels
+    totals1 <- ddply(topBank1, .(Company, monthYr), summarise, freq=length(Company))
+
+    #build plot
+    input$goButton
+    isolate({
+      #if submit button was not clicked, show only data for top 10 banks
+      if (input$goButton == 0) {  
+        p <- ggplot(data=totals1, aes(x=monthYr, y=freq, group = Company, colour = Company)) + 
+          geom_line(size=1) +  
+          geom_point( size=2, shape=21, fill="white") +
+          labs(title = expression("Total Number of Complaints, All Issue Types")) +
+          labs(x = "Date Received") + 
+          labs(y = "Total Numbers of Complaints") +
+          guides(color=guide_legend(title="Bank"))
+        
+        print(p)
+      }
+      #else if submit button was pressed, add the selected bank to the graph
+      else {
+        #build subset of data with top 10 banks + the selected bank
+        topBank1 <- cfpb[,(names(cfpb) %in% c("Company", "monthYr"))]
+        topBank1 <- data.frame(subset(topBank1, Company %in% input$bank | Company %in% largeNum1$bank))
+        
+        #get frequency counts
+        totals1 <- ddply(topBank1, .(Company, monthYr), summarise, freq=length(Company))
+        
+        #create a dynamic graph title
+        graphTitle <- paste("Number of Complaints for", input$bank , "Compared to Top Banks for 2014")
+        
+        #get the last point on the selected bank's line for annotation
+        selectedBank <- input$bank
+        selectedBankData <-data.frame(subset(totals1, Company %in% input$bank))
+        maxMonthYr <- max(selectedBankData$monthYr)
+        maxFreq <- max(subset(selectedBankData, monthYr %in% maxMonthYr)$freq)
+        
+        #build plot
+        p <- ggplot(data=totals1, aes(x=monthYr, y=freq, group = Company, colour = Company)) + 
+          geom_line(size=1) +  
+          geom_point( size=2, shape=21, fill="white") +
+          labs(title =  graphTitle) +
+          labs(x = "Date Received") + 
+          labs(y = "Total Numbers of Complaints") +
+          annotate("text",  group = selectedBank, y=maxFreq, x=maxMonthYr, label = input$bank,colour = "red", size = 3.5)
+
+        print(p)
+      }
+    })
+  })
+  
+  #download data button
+  output$downloadData <- downloadHandler(
+      filename = function() { paste('CFPBData_2014', '.csv', sep='') },
+      content = function(file) {
+        write.csv(datasetInput(), file)
+      }  
+  )
+  
+  #download data table creation
+  datasetInput <- reactive({
+    #if button is clicked, export only selected bank data
+    input$goButton
+    isolate({
+    if (input$goButton == 0) {
+      cfpb
+    }
+    else {
+      subset(cfpb, Company %in% input$bank)
+    }
+    })
+  })
+  
 })
--- ChooseABankAssistant/ui.R
+++ ChooseABankAssistant/ui.R
@@ -1,26 +1,56 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application 
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Looking to Switch Banks?"),
+  titlePanel(img(src="Logo.jpg"), windowTitle="Choose a Bank"),
   
-  # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("plot1"),
-      textOutput("text1")
+      helpText("See all complaints filed with the Consumer
+               Financial Protection Bureau by selecting the bank
+               name below."),
+      br(),
+      br(),
+      #drop-down box
+      uiOutput("choose_bank"),
+      br(),
+      #go button to refresh second graph
+      actionButton("goButton", "Submit"),
+      br(),
+      br(),
+      helpText("Download the data specific to your bank."),
+      br(),
+      #download button
+      downloadButton('downloadData', 'Download'),
+      br(), 
+      br(),
+      h4("Additional Information:"),
+      br(),
+      p("This data has been obtained from the Consumer Financial
+              Protection Bureau (CFPB), more information about the data and data dictionary 
+              can be obtained",
+        a("here.", 
+          href = "http://www.consumerfinance.gov/complaintdatabase/technical-documentation/",target="_blank")),
+      p("The stacked bar chart shows the 10 banks with the most complaints for 2014 broken down by issue type."),
+      p("The line graph shows the total number of complaints for the 10 banks 
+              with the most complaints for 2014."),
+      p("When you select a bank from the drop down and click Submit, the line graph will call out the 
+              bank you selected."),
+      p("If you don't select a bank, the Download button will export all 2014 CFPB data.
+              Once you have selected a bank and click Submit, the Download button will export all 2014 CFPB data 
+              for your selected bank.")
       
+
+      ),
+    
+    mainPanel(
+      #output frequency barplot
+      plotOutput("freqPlot",width="750px",height="300px"), 
+      br(),
+      #output line graph
+      plotOutput("linePlot")
     )
   )
 ))
